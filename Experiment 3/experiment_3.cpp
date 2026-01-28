173dc5e96c56d2195c21288be56e3813ece1682f
iampankaj <pankajsuthar20108@gmail.com>
2026-01-28 22:04:13 +0530

    Add files via upload

---
+++ Experiment 3/experiment_3.cpp
@@ -0,0 +1,27 @@
+//Experiment–3: Stable Attendance Window
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    vector<char>v(n,0);
+    for (int i=0;i<n;i++) {
+        cin >> v[i];
+    }
+    unordered_map<int, int>m;
+    m[0]=-1;
+    int sum=0, maxLen=0;
+    for (int i=0;i<n;i++) {
+        if(v[i]=='P'||v[i]=='p'){
+            sum+=1;}
+        else{
+            sum-=1;}
+        if (m.find(sum)!= m.end()) {
+            maxLen = max(maxLen, i - m[sum]);
+        } else {
+            m[sum]=i;
+        }
+    }
+    cout<<maxLen;
+    return 0;
+}
